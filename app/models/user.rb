4eb814ee59e98388fb2e6b47af90c0ae92ecbf44
kamil <kamil@example.com>
2013-04-27 17:23:30 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  attr_accessible :company, :contact_id, :email, :email_private, :extension, :fax, :firstName, :home_phone, :lastName, :mobile, :notes, :phone, :site_city, :site_phone, :site_zip, :title
+  has_many :tickets
+end
